29732fe405bd3a8d2ded153b4877ec84fd6d37ea
CarlosOX <84168967+CarlosOX@users.noreply.github.com>
2021-08-26 09:14:05 -0500

    Add files via upload

---
+++ Act 1.1. Funciones Iterativas,Recursivas y su análisis de complejidad/Funciones.h
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdlib.h>
+
+
+using namespace std;
+
+class Funciones{
+	public:
+		int sumaIterativa(int);
+		int sumaRecursiva(int);
+		int sumaDirecta(int);
+		
+};
+
+
+int Funciones:: sumaIterativa(int n){
+	int acum=0;
+	
+	for(int i=0;i<=n;i++){
+		acum=acum+i;
+		
+	}
+	return acum;
+}
+
+int Funciones::sumaRecursiva(int n){
+	if(n<=0){
+		return 0;
+		
+	}else {
+		return n+sumaRecursiva(n-1);
+	}
+}
+
+
+int Funciones::sumaDirecta(int n){
+	int suma;
+	suma=n*(n+1)/2;
+	return suma;	
+}
+
+
+
+
+
